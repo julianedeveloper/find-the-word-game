94a0c498507337220c3b8a43ca5eb474ad149f99
julianedeveloper <dev.juliane@gmail.com>
2022-05-31 17:03:51 -0300

    trying the hangman and fixing canva stuffs

--- find-the-word.js
+++ find-the-word.js
@@ -1,29 +1,38 @@
 var tela = document.querySelector('canvas');
 var pincel = tela.getContext('2d');
 
-pincel.fillStyle = 'white';
-pincel.fillRect(200, 50, 600, 400);
-
-console.log('pincel');
-
-pincel.fillStyle = 'darkblue';
-pincel.beginPath();
-pincel.moveTo(100, 100);
-pincel.lineTo(100, 200);
-pincel.fill();
-
-console.log('pincel');
+pincel.fillStyle = 'antiquewhite';
+pincel.fillRect(200, 50, 1200, 500);
+pincel.strokeStyle = 'pink'
+pincel.strokeRect = (250, 50, 1100, 500);
 
 
+function foundationHangMan() {
 
-function startGame() {
+/*
+    pincel.strokeStyle = "#0A3871"; //primeiro definimos a cor das linhas, preferencialmente em formato rgba
+    pincel.lineWidth = 4; //depois o width(espessura) dela
+    pincel.beginPath(); //iniciamos o caminho
+    pincel.moveTo(200, 400); //definimos a movimentação do eixo
+    pincel.lineTo((500+45), 200);  //possiveis intervalos de execução
+    pincel.stroke(); //e por fim o stroke (contorno)
 
-    const theSecretWord = document.querySelector('canvas');
-
-    const words = ['desafio', 'coragem', 'persistir', 'disciplina', 'crer', 'descansar', 'orar', 'meditar', 'conseguir', 'lutar'];
-
-    let random = words[Math.random() * words.length];
-    console.log(random);
-
-    return startGame();
 }
+
+foundationHangMan();
+*/
+
+    pincel.strokeStyle = "#0A3871";
+    pincel.lineWidth = 4;
+    pincel.beginPath();
+    pincel.moveTo(200, 350); //foundation
+    pincel.lineTo(250, 350);
+    pincel.moveTo(50, 300);
+    pincel.lineTo(50, 50);
+    pincel.lineTo(150, 50);
+    pincel.lineTo(150, 80); 
+    pincel.stroke();
+
+}
+
+foundationHangMan();
